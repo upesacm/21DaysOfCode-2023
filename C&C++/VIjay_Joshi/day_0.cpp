a88237b7915ab0350d2d301e59fe3612d5d32db8
Vijay-J0shi <123758166+Vijay-J0shi@users.noreply.github.com>
2023-06-30 22:21:42 +0530

    Update day_0.cpp

--- C&C++/VIjay_Joshi/day_0.cpp
+++ C&C++/VIjay_Joshi/day_0.cpp
@@ -1,19 +1,22 @@
-
 #include <iostream>
 using namespace std;
-
 int main() {
-  int i {0};
-  int j {1};
-  n=i+j;
-  for(int i=0;i<n;i++){
-    if(i>100){
-      break;
-i {j};
-    j {i+j};
-      for(int j=0;j<n;j++){
-        cout<<j<<" ";
-      cout<<endl;
-  }
-  return 0;
+    int rows = 5;
+
+    for (int i = 1; i <= rows; i++) {
+       
+        for (int j = 1; j <= rows - i; j++) {
+            cout << " ";
+        }
+
+  =
+        for (int k = 1; k <= i; k++) {
+            cout << "* ";
+        }
+
+        
+        cout << std::endl;
+    }
+
+    return 0;
 }
