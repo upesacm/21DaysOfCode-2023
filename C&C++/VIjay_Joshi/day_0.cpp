7eb87681085319568893281c290709d8bd7954a8
Vijay-J0shi <123758166+Vijay-J0shi@users.noreply.github.com>
2023-06-30 22:18:48 +0530

    Update day_0.cpp

--- C&C++/VIjay_Joshi/day_0.cpp
+++ C&C++/VIjay_Joshi/day_0.cpp
@@ -1,1 +1,19 @@
 
+#include <iostream>
+using namespace std;
+
+int main() {
+  int i {0};
+  int j {1};
+  n=i+j;
+  for(int i=0;i<n;i++){
+    if(i>100){
+      break;
+i {j};
+    j {i+j};
+      for(int j=0;j<n;j++){
+        cout<<j<<" ";
+      cout<<endl;
+  }
+  return 0;
+}
