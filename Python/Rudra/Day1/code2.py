8e49650b7815ee02a5b5267cfe91c5ff8c10ddb1
rudra <138289028+togoro31@users.noreply.github.com>
2023-07-03 02:35:35 +0530

    Day1code2.py

---
+++ Python/Rudra/Day1/code2.py
@@ -0,0 +1,9 @@
+def reverse_words(sentence):
+    words = sentence.split()
+    reversed_words = [word[::-1] for word in words]
+    reversed_sentence = ' '.join(reversed_words)
+    return reversed_sentence
+
+input_sentence = input()
+reversed_sentence = reverse_words(input_sentence)
+print(reversed_sentence)
