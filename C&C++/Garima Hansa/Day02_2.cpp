40dc91489de0993dc7d64334a08f4094b2f8c310
HanArima <garhanimasa2003@gmail.com>
2023-07-04 02:42:23 +0530

    Day 02

---
+++ C&C++/Garima Hansa/Day02_2.cpp
@@ -0,0 +1,22 @@
+//2. Write a program to swap the values of two variables without using a third variable using the bitwise XOR operator.
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+    cout << "Enter two numbers: ";
+    cin >> a;
+    cin >> b;
+
+    cout << "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"<<endl;
+    cout << "Your numbers are "<<endl<< "a = " << a <<endl << "b = " << b <<endl << endl;
+    
+    int c = a^b;
+    a = a^c;
+    b = b^c;
+
+    cout << "...Swapping values using XOR..." << endl << endl;
+    cout << "Now, your numbers are: "<<endl << "a = " << a <<endl << "b = " << b <<endl;
+    cout << "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"<<endl;
+}
