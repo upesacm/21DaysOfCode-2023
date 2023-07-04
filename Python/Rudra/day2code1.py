a785dc28ac207ef44d32be57e069a7e69c5a20fb
rudra <138289028+togoro31@users.noreply.github.com>
2023-07-04 14:01:24 +0530

    day2code1.py

--- Python/Rudra/day2code1.py
+++
@@ -1,9 +0,0 @@
-def check_even_odd(num):
-    if num % 2 == 0:
-        return "Even"
-    else:
-        return "Odd"
-
-num = int(input("Enter an integer: "))
-result = check_even_odd(num)
-print(f"The number {num} is {result}.")
