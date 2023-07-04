1173e92a59b5df8ad6592706ed4d58ed64821b01
Vijay Joshi <123758166+Vijay-J0shi@users.noreply.github.com>
2023-07-04 21:56:29 +0530

    Create Ques1.cpp

---
+++ C&C++/VIjay_Joshi/day2/Ques1.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int year;
+    cout<<"Enter the year"<<endl;
+    cin>>year;
+
+    if(year%400==0){
+        cout<<"Year "<<year<<" is a leap year";
+    }
+
+    else if(year%4==0 &&year%100!=0){
+        cout<<"Year "<<year<<" is a leap year";
+    }
+    else{
+        cout<<"No "<<year<<" is not a leap year";
+    }
+return 0;
+}
