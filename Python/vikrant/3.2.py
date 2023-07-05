a4f1c9b770aaa7726f38af1267dec0ddce34bc6b
VikrantKuntal1 <99619802+VikrantKuntal1@users.noreply.github.com>
2023-07-05 20:35:53 +0530

    Add files via upload

---
+++ Python/vikrant/3.2.py
@@ -0,0 +1,10 @@
+from array import *
+
+sum = 0
+integer = int(input("Enter an integer: "))
+
+while integer>=0:
+    sum+=integer
+    integer = int(input("Enter an integer: "))
+
+print("The sum of the number is: ", sum)
