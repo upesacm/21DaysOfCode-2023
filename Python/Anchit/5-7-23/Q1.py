441c98a6a13425b63bb255806f90be76557e1047
Anchit Das <115886474+anchitd10@users.noreply.github.com>
2023-07-05 22:07:29 +0530

    Create Q1.py

---
+++ Python/Anchit/5-7-23/Q1.py
@@ -0,0 +1,10 @@
+x=int(input("Enter number 1:"))
+y=int(input("Enter number 2:"))
+z=int(input("Enter number 3:"))
+
+if x<y>z:
+    print(y,"is the largest number")
+elif y<x>z:
+    print(x,"is the largest number")
+else:
+    print(z,"is the largest number")
