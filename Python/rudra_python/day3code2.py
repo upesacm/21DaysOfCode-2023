2cde94a3354291203fe84cbc815647a57214deb9
rudracodeshere <138610693+rudracodeshere@users.noreply.github.com>
2023-07-05 15:38:43 +0530

    Update day3code2.py rudra sharma

--- Python/rudra_python/day3code2.py
+++ Python/rudra_python/day3code2.py
@@ -1,1 +1,1 @@
-
+# Write a Python program that prompts the user to enter a series of integers, one at a time, until they enter a negative number. The program should then terminate and compute and print the sum of all the positive integers entered by the user.
