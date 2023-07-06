faab8839647cfc5ef1f3d16ab9f80c4d1f6ac5b3
SaloniSukirti <salonisukirti1206@gmail.com>
2023-07-07 01:15:19 +0530

    SaloniDay5

---
+++ Python/Saloni_Sukirti/DAY5/score.py
@@ -0,0 +1,11 @@
+#Python
+
+# 1. Write a Python program that takes a student's score as input and determines whether
+# the student has passed or failed. Assume that the passing score is 60 or greater.
+
+num = int(input("Enter the number: "))
+
+if num >= 60:
+    print("The student has passed the test ")
+else:
+    print("The student has failed the test ")
