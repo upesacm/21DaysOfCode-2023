2b772bba520a7783c8753729be08c808f58a08dd
Rudra Sharma <138610693+rudracodeshere@users.noreply.github.com>
2023-07-08 16:32:57 +0530

    Update day6code2.py

--- Python/rudra_python/day6code2.py
+++ Python/rudra_python/day6code2.py
@@ -1,1 +1,13 @@
+def find_second_smallest(numbers):
+    if len(numbers) < 2:
+        return None
+
+    smallest = min(numbers)
+    second_smallest = float('inf')
+
+    for num in numbers:
+        if num > smallest and num < second_smallest:
+            second_smallest = num
+
+    return second_smallest
 
