4e5a67bd9dfe6f24c535aabaa3fb793a5d9a5d7a
Rudra Sharma <138610693+rudracodeshere@users.noreply.github.com>
2023-07-08 16:32:04 +0530

    Update day6code1.py

--- Python/rudra_python/day6code1.py
+++ Python/rudra_python/day6code1.py
@@ -1,1 +1,11 @@
+def find_largest(numbers):
+    if not numbers:
+        return None
+
+    largest = numbers[0]
+    for num in numbers:
+        if num > largest:
+            largest = num
+
+    return largest
 
