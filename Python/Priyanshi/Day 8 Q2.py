0a25fc114174eefcdc1552b0b44828d3fcf5e8b2
priii-25 <121582683+priii-25@users.noreply.github.com>
2023-07-10 21:11:18 +0530

    Update and rename Day 8 Q2 to Day 8 Q2.py

---
+++ Python/Priyanshi/Day 8 Q2.py
@@ -0,0 +1,10 @@
+def calculate_average_grades(students):
+    for student in students:
+        name = student[0]
+        grades = student[1:]
+        avg = sum(grades) / len(grades)
+        r_avg= round(avg, 2)
+        print(f"{name}: {r_avg}")
+li= input("Enter the student data: ")
+students= eval(li)
+calculate_average_grades(students)
