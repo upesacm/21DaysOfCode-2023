49fde25dcfdc01cee0fe46975a5ffebf7d537b9c
geetika212 <88263877+geetika212@users.noreply.github.com>
2023-07-13 23:22:53 +0530

    Create Day11_1.c

---
+++ C&C++/Geetika/Day11_1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+struct Book {
+    char title[100];
+    char author[100];
+    int year;
+};
+
+void details(struct Book book) {
+    printf("Title: %s\n", book.title);
+    printf("Author: %s\n", book.author);
+    printf("Year: %d\n", book.year);
+}
+
+int main() {
+    struct Book myBook;
+  
+    struct Book book = {
+        .title = "To Kill a Mockingbird",
+        .author = "Harper Lee",
+        .year = 1960
+    };
+  
+    myBook = book;
+    details(myBook);
+    return 0;
+}
