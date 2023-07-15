3e744093584a61f5328333f7acea99e5b7bcd58a
Kemist24 <purvanshrathore2410@gmail.com>
2023-07-15 12:13:33 +0530

    Added PYTHON DAY 13 Q1

---
+++ Python/Purvansh Rathore/Day13 Q1.py
@@ -0,0 +1,12 @@
+set1 = {1, 2, 3, 4, 5}
+set2 = {4, 5, 6, 7, 8}
+
+# Print the sets
+print("Set1:", set1)
+print("Set2:", set2)
+
+# Perform the union of set1 and set2
+union_set = set1.union(set2)
+
+# Print the union set
+print("Union of set1 and set2:", union_set)
