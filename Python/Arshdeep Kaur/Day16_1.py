c5ce9654d7c77bf4ec261a1499c469863a4a633c
Arshdeep Kaur <76038495+ArshdeepKaur04@users.noreply.github.com>
2023-07-18 21:57:21 +0530

    Create Day16_1.py

---
+++ Python/Arshdeep Kaur/Day16_1.py
@@ -0,0 +1,8 @@
+#Create a NumPy array of shape (3, 3) filled with random integers between 1 and 10. Print the array with a descriptive message.
+
+import numpy as np
+
+random_array = np.random.randint(1, 11, size=(3, 3))
+
+print("Random array of shape (3, 3) filled with integers between 1 and 10 is:")
+print(random_array)
