06ca14a7cfcadd4e71dc0a47049a405950c7773d
avnigoyal25 <500090996@stu.upes.ac.in>
2023-07-22 12:46:34 +0530

    day20 Avni Goyal

---
+++ C&C++/Avni/day20/1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+float pi=3.14159;
+
+int main()
+{
+    int r;
+    cout<<"enter the radius"<<endl;
+    cin>>r;
+    cout<<"circumference is "<<2*r*pi<<endl;
+}
