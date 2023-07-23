3f884792f433a67ccdea4c0b428f9cdc53de8b8e
Vijay Joshi <123758166+Vijay-J0shi@users.noreply.github.com>
2023-07-23 10:01:15 +0530

    Update ques1.cpp

--- C&C++/VIjay_Joshi/day21/ques1.cpp
+++ C&C++/VIjay_Joshi/day21/ques1.cpp
@@ -4,9 +4,14 @@ using namespace std;
 int main() {
     int n;
 
-    cout << "Enter a number to generate  multiplication table : ";
+    cout << "Enter a number to generate  multiplication table (1-10): ";
     cin >> n;
 
+    
+if (cin.fail() || number < 1 || number > 10) {
+        cout << "Invalid input" << endl;
+        return 1;
+    }
 
     cout << "Multiplication Table of " << n << ":" << endl;
     for (int i = 1; i <= 10; i++) {
