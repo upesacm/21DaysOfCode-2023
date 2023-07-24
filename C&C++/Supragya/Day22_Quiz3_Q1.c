8530fad2f4df7768fdd30c55975544d7eb12b77f
Supragya <125485615+Supragya1@users.noreply.github.com>
2023-07-24 23:26:53 +0530

    Create Day22_Quiz3_Q1.c

---
+++ C&C++/Supragya/Day22_Quiz3_Q1.c
@@ -0,0 +1,20 @@
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+def primes(n):
+    primes = []
+    num = 2
+    while len(primes) < n:
+        if is_prime(num):
+            primes.append(num)
+        num += 1
+    return primes
+
+n = int(input())
+prime_numbers = primes(n)
+print(*prime_numbers)
