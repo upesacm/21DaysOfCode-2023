c330f4626d08b3f027d73e06f7363626e850f32b
geetika212 <88263877+geetika212@users.noreply.github.com>
2023-07-24 22:04:11 +0530

    Create Quiz3_2.c

---
+++ C&C++/Geetika/Quiz3_2.c
@@ -0,0 +1,50 @@
+// Merge two sorted quantum states, representing equal-sized arrays, into a new sorted quantum state with double the elements. 
+
+#include <stdio.h>
+
+void mergeArrays(int n, int arr1[], int arr2[], int result[]) {
+    int i = 0, j = 0, k = 0;
+    
+    // Compare elements of both arrays and merge them in ascending order
+    while (i < n && j < n) {
+        if (arr1[i] < arr2[j]) {
+            result[k++] = arr1[i++];
+        } else {
+            result[k++] = arr2[j++];
+        }
+    }
+    
+    // Copy any remaining elements from arr1
+    while (i < n) {
+        result[k++] = arr1[i++];
+    }
+    
+    // Copy any remaining elements from arr2
+    while (j < n) {
+        result[k++] = arr2[j++];
+    }
+}
+
+int main() {
+    int n;
+    
+    scanf("%d", &n);
+    
+    int arr1[n], arr2[n], result[2 * n];
+    
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr1[i]);
+    }
+    
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr2[i]);
+    }
+    
+    mergeArrays(n, arr1, arr2, result);
+    
+    for (int i = 0; i < 2 * n; i++) {
+        printf("%d ", result[i]);
+    }
+    
+    return 0;
+}
