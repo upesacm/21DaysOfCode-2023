38e22ab54f56d34097de81ac777840774da40425
geetika212 <88263877+geetika212@users.noreply.github.com>
2023-07-24 23:02:21 +0530

    Create Quiz3_5.c

---
+++ C&C++/Geetika/Quiz3_5.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int binarySearch(int arr[], int left, int right, int target) {
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        
+        if (arr[mid] == target) {
+            return mid;
+        } else if (arr[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    
+    return -1;
+}
+
+int main() {
+    int n, target;
+    scanf("%d", &n);
+    
+    int arr[n];
+    
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    
+    scanf("%d", &target);
+    
+    int index = binarySearch(arr, 0, n - 1, target);
+    
+    printf("%d\n", index);
+    
+    return 0;
+}
